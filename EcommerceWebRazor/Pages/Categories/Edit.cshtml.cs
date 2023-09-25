be05d4b0e50c1bc507fd5338e68640232eaaa6f3
Mert Karaibrahimoğlu <mertkaraibrahimoglu@outlook.com>
2023-09-25 15:55:41 +0300

    razor project added

---
+++ EcommerceWebRazor/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,40 @@
+using EcommerceWebRazor.Data;
+using EcommerceWebRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EcommerceWebRazor.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int? id)
+        {
+            if(id != null || id!= 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited!";
+                return RedirectToAction("Index");
+            }
+            return Page();
+        }
+    }
+}
