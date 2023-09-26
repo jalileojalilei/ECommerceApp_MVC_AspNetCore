88b2a44e36a9fcbf98eb1e433248d650138adc29
Mert Karaibrahimoğlu <mertkaraibrahimoglu@outlook.com>
2023-09-26 15:08:27 +0300

    repository

---
+++ EcommerceDataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using EcommerceDataAccess.Data;
+using EcommerceDataAccess.Repository.IRepository;
+using EcommerceModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceDataAccess.Repository
+{
+    internal class CategoryRepository : Repository<Category>,  ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
